2c6c13d6ec17d27aa406a17ac9d1e22e72b11ef2
Aaron Rodriguez <melky.up@gmail.com>
2024-07-28 21:10:57 -0700

    change output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://melkyy.github.io',
+  output: "static",
     integrations: [tailwind(), mdx(), icon({
     include: {
       mdi: ["*"]
